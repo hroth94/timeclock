c94da4022caec18610d0d4a5f33ecb2731700489
Hannah R <hroth94@vt.edu>
2016-12-11 20:16:29 -0500

    Changes to clock in/out

---
+++ timeclock_v1/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// path constants
+define('SYSTEM_PATH', dirname(__FILE__));
+
+// database constants
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_DATABASE','timeclock');
--- timeclock_v1/model/Time.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Time {
-  public function getCurrentTime() {
-    return date('l jS \of F Y h:i:s A');
-  }
-}
-
-?>
